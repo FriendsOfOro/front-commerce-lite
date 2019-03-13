34e095fc5d2e65df6b271c1a59b85328cbaa9c0f
Pierre Martin <pierre@occitech.fr>
2019-03-13 13:23:38 +0100

    Add interceptor to regenerate the WSSE token on each call
    … and add product single query

--- src/server/modules/oro/resolvers.js
+++ src/server/modules/oro/resolvers.js
@@ -2,19 +2,24 @@ import wsse from "wsse";
 import axios from "axios";
 const JSONAPIDeserializer = require("jsonapi-serializer").Deserializer;
 
-const token = wsse({
-  username: "admin",
-  password: process.env.ORO_ADMIN_API_KEY
-});
-
 const axiosInstance = axios.create({
   baseURL: "https://demo.orocommerce.com/",
-  timeout: 10000,
-  headers: {
-    "Content-type": "application/vnd.api+json",
-    "X-WSSE": token.getWSSEHeader({ nonceBase64: true }),
-    Authorization: 'WSSE profile="UsernameToken"'
-  }
+  timeout: 10000
+});
+axiosInstance.interceptors.request.use(config => {
+  const token = wsse({
+    username: "admin",
+    password: process.env.ORO_ADMIN_API_KEY
+  });
+
+  return {
+    ...config,
+    headers: {
+      "Content-type": "application/vnd.api+json",
+      "X-WSSE": token.getWSSEHeader({ nonceBase64: true }),
+      Authorization: 'WSSE profile="UsernameToken"'
+    }
+  };
 });
 
 export default {
@@ -31,10 +36,37 @@ export default {
           layer: {
             products: new JSONAPIDeserializer().deserialize(data)
           }
-        }))
-        .catch(e => console.log(e))
+        })),
+    product: (_, { sku }) =>
+      axiosInstance
+        .get("admin/api/products", {
+          params: {
+            include: "names",
+            "filter[sku]": sku
+          }
+        })
+        .then(({ data }) => new JSONAPIDeserializer().deserialize(data))
+        .then(products => (products.length ? products[0] : null))
   },
   Product: {
-    name: ({ names }) => names[0].string
+    name: ({ names }) => names[0].string,
+    prices: () => {
+      const fakeValue = {
+        amount: 42.99,
+        currency: "USD"
+      };
+      return {
+        finalPrice: {
+          priceInclTax: {
+            value: fakeValue,
+            includeTax: true
+          },
+          priceExclTax: {
+            value: fakeValue,
+            includeTax: false
+          }
+        }
+      };
+    }
   }
 };
