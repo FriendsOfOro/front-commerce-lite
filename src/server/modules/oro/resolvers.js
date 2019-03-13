ad9d0c4b60bf9f69e0a9e4a73922811a9147ebe5
Pierre Martin <pierre@occitech.fr>
2019-03-13 11:08:01 +0100

    POC Admin Authentication

--- src/server/modules/oro/resolvers.js
+++ src/server/modules/oro/resolvers.js
@@ -1,5 +1,27 @@
+import wsse from "wsse";
+import axios from "axios";
+
+const token = wsse({
+  username: "admin",
+  password: process.env.ORO_ADMIN_API_KEY
+});
+
+const axiosInstance = axios.create({
+  baseURL: "https://demo.orocommerce.com/",
+  timeout: 10000,
+  headers: {
+    "Content-type": "application/json",
+    "X-WSSE": token.getWSSEHeader({ nonceBase64: true }),
+    Authorization: 'WSSE profile="UsernameToken"'
+  }
+});
+
 export default {
   Query: {
-    hello: () => "world"
+    hello: () =>
+      axiosInstance
+        .get("admin/api/countries")
+        .then(({ data }) => JSON.stringify(data, null, 2))
+        .catch(e => console.log(e))
   }
 };
